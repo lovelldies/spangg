15c01e254a5af39273d5a9c481ac71aec9b67016
Lovell D'souza <lovelldies@gmail.com>
2017-11-23 11:27:29 +0530

    Splitting code
    Splitting the code into modules since we are going to have more pages.

---
+++ inc/header.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en"> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="Spangg is a digital marketing agency &mdash; experts at helping B2C brands leverage social media and online advertising to drive their business forward.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="<?php echo $pageTitle; ?>" />
+    <meta property="og:description" content="Spangg is a digital marketing agency &mdash; experts at helping B2C brands leverage social media and online advertising to drive their business forward." />
+    <meta property="og:url" content="http://spangg.com" />
+    <meta property="og:image" content="http://spangg.com/img/hello-spangg.png" />
+
+    <title><?php echo $pageTitle; ?></title>
+  
+    <link rel="apple-touch-icon" href="img/favicon.png">
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+    <link rel="author" href="humans.txt" />
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+  </head>
+  <body>
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+    <!-- Facebook tracking code -->
+	<div id="fb-root"></div>
+    <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.4&appId=1481515385499302";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+	
+	<!-- Google Analytics -->
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-67992235-1', 'auto');
+    ga('send', 'pageview');
+    </script>
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+$pageTitle = 'Spangg &mdash; Digital Marketing Agency';
+
+include_once './inc/header.php';
+?>
+   
+  <div class="container">
+    <div class="row">
+      <img class="center-block img-responsive" src="img/hello-spangg.png" alt="Hello, we're Spangg!" title="Spangg - Digital Marketing Agency" />
+    </div>
+  </div>
+
+  <div class="jumbotron">
+    <div class="container">
+      <center>
+      <h2>we're experts at helping B2C brands achieve their business goals</h2>
+      <p>Our digital marketing expertise has been appreciated by the best and the brightest. Click on one of the blue buttons, below, to see what we're great at, and some of the stuff we've done.</p>
+      <p><a class="btn spangg-button btn-lg" href="https://drive.google.com/uc?export=download&id=0B-vTv-KRpo43S1FDbHVnZVc5SUxpU2Q4VXdCRVY1LXplLXNJ" role="button">Download <small>(16MB)</small> &raquo;</a> <a role="button" class="btn spangg-button btn-lg" target="_blank" href="https://drive.google.com/file/d/0B-vTv-KRpo43S1FDbHVnZVc5SUxpU2Q4VXdCRVY1LXplLXNJ/view?usp=sharing">View Online</a></p>
+      </center>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <center>
+      <h2>we can help you achieve your business goals</h2>
+      <p>Spangg means 'exactly', and reflects the quality of our work. Get in touch with us to see how we can work together.</p>
+      <p><a class="btn spangg-button btn-lg" href="mailto:know@spangg.com" target="_blank" role="button">Email</a> <a role="button" class="btn spangg-button btn-lg" target="_blank" href="tel:+91 9985 989 300">Call</a> <a role="button" class="btn spangg-button btn-lg" target="_blank" href="https://www.facebook.com/SpanggMedia/">Facebook</a></p>
+      </center>
+    </div>
+  </div>
+
+<?php include_once './inc/footer.php'; ?>
