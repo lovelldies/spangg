ef8b4da3ea4fbf07c17efc35a461396f2adcc7a1
Lovell D'souza <lovelldies@gmail.com>
2017-11-28 14:57:58 +0530

    Added "Work" content

---
+++ work.php
@@ -0,0 +1,22 @@
+<?php 
+$pageTitle = 'About Spangg &mdash; Digital Marketing Agency';
+
+include_once './inc/header.php';
+?>
+
+<div class="container" id="work">
+<img src="./img/work-banner.png" alt="Work by Spangg!" class="img-responsive">
+</div>
+
+<div class="jumbotron">
+  <div class="container-fluid">
+    <center>
+    <h2>we're experts at helping B2C brands achieve their business goals</h2>
+    <p>Click on one of the blue buttons, below, to see a soon-to-be updated deck of some of the stuff we've done.</p>
+    <p><a class="btn spangg-button btn-lg" href="https://drive.google.com/uc?export=download&amp;id=0B-vTv-KRpo43S1FDbHVnZVc5SUxpU2Q4VXdCRVY1LXplLXNJ" role="button">Download <small>(16MB)</small> »</a> <a role="button" class="btn spangg-button btn-lg" target="_blank" href="https://drive.google.com/file/d/0B-vTv-KRpo43S1FDbHVnZVc5SUxpU2Q4VXdCRVY1LXplLXNJ/view?usp=sharing">View Online</a></p>
+    </center>
+  </div>
+</div>
+
+
+<?php include_once './inc/footer.php'; ?>
