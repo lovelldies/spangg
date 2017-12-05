6c3c50616e760fa63e4e377092c6833ec868a6bb
Lovell D'souza <lovelldies@gmail.com>
2017-12-05 18:39:29 +0530

    Updated the contact form

--- inc/contact_form.php
+++ inc/contact_form.php
@@ -8,32 +8,27 @@
   <form id="email-form">
   <input type="hidden" name="sendto" value="know@spangg.com">
   <div class="col-md-12 form-group">
-    <label for="name">Name *</label>
-    <input type="text" class="form-control" name="name" id="name" placeholder="Your name" required="true">
+    <input type="text" class="form-control" name="name" id="name" placeholder="* Name" required="true">
   </div>
   <div class="col-md-6 col-sm-6 form-group">
-    <label for="email">Email *</label>
-    <input type="email" class="form-control" name="email" id="email" placeholder="Your email address" required="true">
+    <input type="email" class="form-control" name="email" id="email" placeholder="* Email" required="true">
   </div>
   <div class="col-md-6 col-sm-6 form-group">
-    <label for="phone">Phone</label>
-    <input type="tel" class="form-control" name="phone" id="phone" placeholder="Your phone number">
+    <input type="tel" class="form-control" name="phone" id="phone" placeholder="* Phone">
   </div>
   <div class="col-md-12 form-group">
-    <label for="subject">Subject *</label>
-    <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject"  required="true">
+    <input type="text" class="form-control" id="subject" name="subject" placeholder="* Subject"  required="true">
   </div>
   <div class="col-md-12 form-group">
-    <label for="message">Message *</label>
-    <textarea class="form-control" rows="4" name="message" id="message" placeholder="Please leave us a message." required="true"></textarea>
+    <textarea class="form-control" rows="4" name="message" id="message" placeholder="* Please leave us a message." required="true"></textarea>
   </div>
   <div class="col-md-12">
-    <p class="text-muted"><strong>*</strong> Required fields.</p>
+    <small class="text-muted"><strong>*</strong> Required fields.</small>
   </div>
   <div class="col-md-12 form-group">
     <center>
       <div id="response"></div>
-      <input type="submit" value="Submit" class="btn btn-primary btn-lg" />
+      <input type="submit" value="Submit" class="btn btn-lg spangg-button-orange" />
       <input type="reset" value="Reset" class="btn btn-lg" />
     </center>
   </div>
